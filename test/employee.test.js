5e50ebbf8d271333cd404ca7931ed7549a9e56db
nbur4556 <nbur4556@gmail.com>
2020-09-26 14:03:13 -0500

    setup tests for all employee methods

--- test/employee.test.js
+++ test/employee.test.js
@@ -1,5 +1,6 @@
 const Employee = require('../lib/employee.js');
 
+// Test Employee object
 describe('Employee', () => {
     // Test for variable type
     it('Creating Employee object should initialize id as a number', () => {
@@ -22,30 +23,49 @@ describe('Employee', () => {
         const employee = new Employee(0, 'name', 'role', 'summary', 'email');
         expect(employee.email).toEqual(typeof String);
     });
+});
 
-    // Throw error on wrong variable type
-    it('Creating Employee object with string as id should throw error', () => {
-        const employee = new Employee('id', 'name', 'role', 'summary', 'email');
-        // expect(emloyee.id).toError('error')
+// Test Employee.getId() method
+describe('Employee getId', () => {
+    it('Should return the id of the object', () => {
+        const testId = 5;
+        const employee = new Employee(testId, 'name', 'role', 'summary', 'email');
+        expect(employee.getId()).toEqual(5);
     });
 });
 
-describe('Employee getId', () => {
-
-});
-
+// Test Employee.getName() method
 describe('Employee getName', () => {
-
+    it('Should return the name of the object', () => {
+        const testName = 'John Doe';
+        const employee = new Employee(0, testName, 'role', 'summary', 'email');
+        expect(employee.getName()).toEqual('John Doe');
+    });
 });
 
+// Test Employee.getRole() method
 describe('Employee getRole', () => {
-
+    it('Should return the role of the object', () => {
+        const testRole = 'employee';
+        const employee = new Employee(0, 'name', testRole, 'summary', 'email');
+        expect(employee.getRole()).toEqual('employee');
+    });
 });
 
+// Test Employee.getSummary() method
 describe('Employee getSummary', () => {
-
+    it('Should return the summary of the object', () => {
+        const testSummary = 'sum';
+        const employee = new Employee(0, 'name', 'role', testSummary, 'email');
+        expect(employee.getSummary()).toEqual('sum');
+    });
 });
 
+// Test Employee.getEmail() method
 describe('Employee getEmail', () => {
-
+    it('Should return the email of the object', () => {
+        const testEmail = 'johndoe@hotmail.com';
+        const employee = new Employee(0, 'name', 'role', 'summary', testEmail);
+        expect(employee.getEmail()).toEqual('johndoe@hotmail.com');
+    });
 });
