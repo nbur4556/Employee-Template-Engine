c2264a2743e899303c43f33a99cc02e11d0309ba
nbur4556 <nbur4556@gmail.com>
2020-09-26 14:09:37 -0500

    adds test for engineer class

---
+++ test/engineer.test.js
@@ -0,0 +1,27 @@
+const Engineer = require('../lib/engineer.js');
+
+// Test Engineer object
+describe('Engineer', () => {
+    // Test for variable type
+    it('Creating Engineer object should initialize gitUsername as a string', () => {
+        const engineer = new Engineer(0, 'name', 'summary', 'email', 'git-username');
+        expect(engineer.gitUsername).toEqual(typeof String);
+    });
+});
+
+// Test Engineer.getGithubUsername() method
+describe('Employee getGithubUsername', () => {
+    it('Should return the githubUsername of the object', () => {
+        const testGithubUsername = 'newuser1234'
+        const engineer = new Engineer(0, 'name', 'summary', 'email', testGithubUsername);
+        expect(engineer.getGithubUsername()).toEqual('newuser1234');
+    });
+});
+
+// Test Engineer.getRole() method
+describe('Employee getRole', () => {
+    it('Should return the role of the object', () => {
+        const engineer = new Engineer(0, 'name', 'summary', 'email', 'git-username');
+        expect(engineer.getRole()).toEqual('engineer');
+    });
+});
