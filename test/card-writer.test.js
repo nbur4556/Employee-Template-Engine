4d8625a6b1e6e9605e99cf12ae5c89118100d67c
nbur4556 <nbur4556@gmail.com>
2020-09-28 06:54:23 -0500

    set tests on initial card writer object

---
+++ test/card-writer.test.js
@@ -0,0 +1,16 @@
+const CardWriter = require('../lib/card-writer.js');
+const Engineer = require('../lib/engineer.js');
+const Intern = require('../lib/intern.js');
+const Manager = require('../lib/manager.js');
+
+describe("Card Writer", () => {
+    it('Can initialize instance of card writer object.', () => {
+        cardWriter = new CardWriter();
+        expect(typeof (cardWriter)).toBe('object');
+    });
+    it('Initalizing card writer object with string parameter sets template', () => {
+        let str = "Testing"
+        cardWriter = new CardWriter(str);
+        expect(cardWriter.template).toEqual(str);
+    });
+});
--- test/make-card.test.js
+++
@@ -1,6 +0,0 @@
-const CardWriter = require('../lib/make-card.js');
-const Engineer = require('../lib/engineer.js');
-const Intern = require('../lib/intern.js');
-const Manager = require('../lib/manager.js');
-
-describe('Card Writer')
