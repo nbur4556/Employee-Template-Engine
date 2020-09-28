8826b5e465525f7693671d7d030780c7e5c06195
nbur4556 <nbur4556@gmail.com>
2020-09-28 07:41:17 -0500

    gets user input for generic employee

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const inquirer = require('inquirer');
+const Engineer = require('./lib/engineer.js');
+const Intern = require('./lib/intern.js');
+const Manager = require('./lib/manager.js');
 
 inquirer.prompt({
     message: 'Select employee role',
@@ -10,5 +13,34 @@ inquirer.prompt({
         'intern'
     ]
 }).then(({ role }) => {
-    console.log(role);
-}); 
+    createEmployee(role);
+});
+
+async function createEmployee(role) {
+    let { id } = await inquirer.prompt({
+        message: 'Enter employee id',
+        name: 'id',
+        type: 'input'
+    });
+    let { name } = await inquirer.prompt({
+        message: 'Enter employee name',
+        name: 'name',
+        type: 'input'
+    });
+    let { summary } = await inquirer.prompt({
+        message: 'Enter employee summary',
+        name: 'summary',
+        type: 'input'
+    });
+    let { email } = await inquirer.prompt({
+        message: 'Enter employee email',
+        name: 'email',
+        type: 'input'
+    });
+
+    console.log(`ID: ${id}`);
+    console.log(`NAME: ${name}`);
+    console.log(`ROLE: ${role}`);
+    console.log(`SUMMARY: ${summary}`);
+    console.log(`EMAIL: ${email}`);
+}
